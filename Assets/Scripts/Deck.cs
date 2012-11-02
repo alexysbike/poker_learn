a5bf3a28e035b6d93a0fe2bc48f4103d0af9113f
Alexys Gonzalez <alexysbike@gmail.com>
2012-11-01 23:13:19 -0430

    nuevo

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -1,46 +1,41 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System;
 
 public class Deck {
 
-	Card[] cards;
+	List<Card> cards = new List<Card>();
 	int discarted;
 	
 	public Deck(){
-		this.cards = new Card[52];
 		InitializeDeck();
 	}
 	
 	public void InitializeDeck(){
+		cards.Clear();
 		int number;
-		for(int i=0; i<(cards.Length/4); i++){
+		for(int i = 0; i<52; i++){
+			cards.Add (new Card());
+		}
+		for(int i=0; i<(cards.Count/4); i++){
 			number = i+1;
-			cards[i] = new Card();
-			cards[(number*2)-1]=new Card();
-			cards[(number*3)-1]=new Card();
-			cards[(number*4)-1]=new Card();
 			cards[i].SetCard(number, Type.Clover);
-			cards[(number*2)-1].SetCard(number, Type.Diamond);
-			cards[(number*3)-1].SetCard(number, Type.Heart);
-			cards[(number*4)-1].SetCard(number, Type.Pike);
+			cards[i+13].SetCard(number, Type.Diamond);
+			cards[i+26].SetCard(number, Type.Heart);
+			cards[i+39].SetCard(number, Type.Pike);
 		}
 		discarted = 0;
 	}
 	
 	public void SortDeck(){
-		System.Random rand = new System.Random();
-		Array.Sort<Card>(cards, delegate(Card x, Card y) {
-			return (x.Equals(y))?0:rand.Next(-1, 2);
-		});
-		/*Card switcher;
-		int random;
-		for(int i = 0; i<cards.Length; i++){
-			random = Random.Range (0,51);
-			switcher = cards[i];
-			cards[i] = cards[random];
-			cards[random] = switcher;
-		}*/
+		cards.Shuffle();
+		cards.Shuffle();
+		cards.Shuffle();
+		cards.Shuffle();
+		cards.Shuffle();
+		cards.Shuffle();
+		
 	}
 	
 	public Card DrawCard(){
@@ -52,5 +47,4 @@ public class Deck {
 	public int CurrrentsCard(){
 		return 52-discarted;
 	}
-	
 }
